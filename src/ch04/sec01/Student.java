c842b51817d01fa571d5c8dda7eff16130f19778
kimyongjun413 <kimyongjun413@gmail.com>
2019-03-29 23:42:58 +0900

    ch04 상속과 리플랙션 - sec01 클래스 확장 - 상속과 기본 메서드

--- src/ch04/sec01/Student.java
+++ src/ch04/sec01/Student.java
@@ -1,6 +1,11 @@
 package ch04.sec01;
 
-public class Student extends Person {
+/*
+* 항상 슈퍼클래스 구현이 인터페이스 구현보다 우선한다.
+* '클래스 우선' 규칙은 자바7과 호환성을 보장한다. 따라서 인터페이스에 기본 메서드를 추가해도
+* 기본 메서드가 생기기 전부터 작동하던 코드에는 영향을 주지 않는다.
+* */
+public class Student extends Person implements Named {
     private  int id;
 
     public Student(String name, int id) {
