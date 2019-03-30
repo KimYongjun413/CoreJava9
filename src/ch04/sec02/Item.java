c24b928476f9e119e2ddf16f85d2ebb33272738d
kimyongjun413 <kimyongjun413@gmail.com>
2019-03-31 01:21:15 +0900

    ch04 상속과 리플랙션 - sec02 Object:보편적 슈퍼클래스 - hashCode 메서드

--- src/ch04/sec02/Item.java
+++ src/ch04/sec02/Item.java
@@ -24,5 +24,7 @@ public class Item {
                 && price == other.price;
     }
 
+    // equals 메서드를 재정의한다면 hashCode 메서드도 재정의해서 equals와 호환되게 해야 한다.
+    // 이렇게 하지 않으면 클래스의 사용자가 해시 집합이나 해시 맵에 객체를 넣다가 잃을 수도 있다.
     public int hashCode() { return Objects.hash(description, price);}
 }
